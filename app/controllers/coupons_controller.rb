ee16b30225df6d7fa3c4ebb74d606b11b0a76c88
Srija S <srijakumar7@gmail.com>
2019-01-18 02:27:04 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,5 +4,20 @@ class CouponsController < ActiveRecord::Base
     @coupons = Coupon.all
   end
 
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code]= params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
 
 end
